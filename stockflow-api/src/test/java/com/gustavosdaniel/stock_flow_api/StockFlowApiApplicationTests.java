895b49353fa401619acb79979227369bdabc3114
Gustavo Silva Daniel <gustavosdaniel@hotmail.com>
2026-04-24 21:09:20 -0300

    feat: iniciando infra estrutura inicial do projeto

---
+++ stockflow-api/src/test/java/com/gustavosdaniel/stock_flow_api/StockFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosdaniel.stock_flow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
