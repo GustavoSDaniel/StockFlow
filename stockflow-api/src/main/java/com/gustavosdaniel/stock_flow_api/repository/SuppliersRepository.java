cefb14b0e6b3aea4bc12518dd7ffdb3eb9708899
Gustavo Silva Daniel <gustavosdaniel@hotmail.com>
2026-06-04 12:56:56 -0300

    feat: iniciando o supplierService

--- stockflow-api/src/main/java/com/gustavosdaniel/stock_flow_api/repository/SuppliersRepository.java
+++ stockflow-api/src/main/java/com/gustavosdaniel/stock_flow_api/repository/SuppliersRepository.java
@@ -2,8 +2,11 @@ package com.gustavosdaniel.stock_flow_api.repository;
 
 import com.gustavosdaniel.stock_flow_api.domain.po.Supplier;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface SuppliersRepository extends R2dbcRepository<Supplier, UUID> {
+
+    Mono<Boolean> existsByCnpj(String cnpj);
 }
--- stockflow-api/src/main/java/com/gustavosdaniel/stock_flow_api/service/SupplierService.java
+++ stockflow-api/src/main/java/com/gustavosdaniel/stock_flow_api/service/SupplierService.java
@@ -1,7 +1,44 @@
 package com.gustavosdaniel.stock_flow_api.service;
 
+import com.gustavosdaniel.stock_flow_api.domain.dto.request.AddressRequest;
+import com.gustavosdaniel.stock_flow_api.domain.dto.request.SupplierContactRequest;
+import com.gustavosdaniel.stock_flow_api.domain.dto.request.SupplierRequest;
+import com.gustavosdaniel.stock_flow_api.domain.dto.response.SupplierResponse;
+import com.gustavosdaniel.stock_flow_api.exception.NameExistException;
+import com.gustavosdaniel.stock_flow_api.repository.AddressRepository;
+import com.gustavosdaniel.stock_flow_api.repository.SupplierContactRepository;
+import com.gustavosdaniel.stock_flow_api.repository.SuppliersRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class SupplierService {
+
+    private final Logger log = LoggerFactory.getLogger(SupplierService.class);
+    private final AddressRepository addressRepository;
+    private final SupplierContactRepository supplierContactRepository;
+    private final SuppliersRepository suppliersRepository;
+
+
+    public SupplierService(AddressRepository addressRepository, SupplierContactRepository supplierContactRepository, SuppliersRepository suppliersRepository) {
+        this.addressRepository = addressRepository;
+        this.supplierContactRepository = supplierContactRepository;
+        this.suppliersRepository = suppliersRepository;
+    }
+
+    public Mono<SupplierResponse> createSupplier(
+            SupplierRequest supplierRequest,
+            SupplierContactRequest supplierContactRequest,
+            AddressRequest addressRequest
+    ){
+        return suppliersRepository.existsByCnpj(supplierRequest.cnpj())
+                .flatMap( existeCnpj -> {
+
+                    if (existeCnpj) Mono.error(new CnpjExisteException);
+                    
+                }
+
+    })
 }
