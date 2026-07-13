bf283c7284ebc81989d10db65d7b9d2e641d8148
Gustavo Silva Daniel <gustavosdaniel@hotmail.com>
2026-07-13 19:40:05 -0300

    test: criando dashboard de teste do overview

--- stockflow-api/src/test/java/com/gustavosdaniel/stock_flow_api/service/OverviewDashboardServiceTest.java
+++ stockflow-api/src/test/java/com/gustavosdaniel/stock_flow_api/service/OverviewDashboardServiceTest.java
@@ -1,16 +1,22 @@
 package com.gustavosdaniel.stock_flow_api.service;
 
 import com.gustavosdaniel.stock_flow_api.domain.dto.response.dashboard.DashboardOverviewResponse;
+import com.gustavosdaniel.stock_flow_api.domain.enums.NotificationPriority;
 import com.gustavosdaniel.stock_flow_api.repository.*;
+import com.gustavosdaniel.stock_flow_api.util.cache.DashboardCacheKeys;
 import com.gustavosdaniel.stock_flow_api.util.cache.DashboardCacheManager;
+import org.checkerframework.checker.units.qual.N;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -54,6 +60,15 @@ class OverviewDashboardServiceTest {
         BigDecimal potentialSalesValue = BigDecimal.valueOf(462800);
         BigDecimal averageMarginPercentage = BigDecimal.valueOf(106800);
 
+        Long totalSuppliers = 22L;
+        Long totalCategories = 13L;
+        Long totalNotifications = 67L;
+
+        NotificationPriority notificationPriority = NotificationPriority.CRITICAL;
+        NotificationPriority notificationPriority1 = NotificationPriority.HIGH;
+
+        List<NotificationPriority> priorities = List.of(notificationPriority, notificationPriority1);
+
         DashboardOverviewResponse.ProductStats productStats =
                 new DashboardOverviewResponse.ProductStats(
                     total, active, inactive, discontinued
@@ -63,6 +78,34 @@ class OverviewDashboardServiceTest {
                 new DashboardOverviewResponse.FinancialStats(
                     totalStockValue, potentialSalesValue, averageMarginPercentage
                 );
+
+        when(productRepository.getDashboardProductStats()).thenReturn(Mono.just(productStats));
+        when(stockRepository.getDashboardFinancialStats()).thenReturn(Mono.just(financialStats));
+        when(suppliersRepository.count()).thenReturn(Mono.just(totalSuppliers));
+        when(categoryRepository.count()).thenReturn(Mono.just(totalCategories));
+        when(notificationRepository.countByNotificationPriorityInAndResolvedFalse(priorities))
+                .thenReturn(Mono.just(totalNotifications));
+        when(cacheManager.getOrCompute(eq(DashboardCacheKeys.OVERVIEW), any(Mono.class)))
+                .thenAnswer(invocation -> invocation.getArgument(1));
+
+        Mono<DashboardOverviewResponse> output = overviewDashboardService.getOverviewDashboard();
+
+        StepVerifier.create(output)
+                .assertNext(resultado -> {
+                    assertNotNull(resultado.products());
+                    assertNotNull(resultado.financials());
+                    assertNotNull(resultado.totalSuppliers());
+                    assertNotNull(resultado.totalCategories());
+                    assertNotNull(resultado.totalNotifications());
+                })
+                .verifyComplete();
+
+        verify(productRepository, times(1)).getDashboardProductStats();
+        verify(stockRepository, times(1)).getDashboardFinancialStats();
+        verify(suppliersRepository).count();
+        verify(categoryRepository).count();
+        verify(notificationRepository).countByNotificationPriorityInAndResolvedFalse(priorities);
+
     }
 
 }
