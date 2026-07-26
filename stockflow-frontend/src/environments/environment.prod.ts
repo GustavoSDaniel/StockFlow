b5cce7fb25eea778d400a14161c03833e6fb939a
Gustavo Silva Daniel <gustavosdaniel@hotmail.com>
2026-07-26 10:31:57 -0300

    feat: atualizando o caminho para apontar pra infra

--- stockflow-frontend/src/environments/environment.prod.ts
+++ stockflow-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:6060',
+  apiUrl: 'https://api.stockflow.gustavosdaniel.com', 
   keycloak: {
-    url: 'http://localhost:6062',
+    url: 'http://34.201.172.14:6062', 
     realm: 'stock-flow-realm',
-    clientId: 'stock-flow-app',
-  },
+    clientId: 'stock-flow-app' 
+  }
 };
